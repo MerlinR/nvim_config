ae67cfa5a75282ac9ee5843e686498c858e67f83
Merlin Roe <merlin.roe@hotmail.co.uk>
2023-11-27 16:21:54 +0000

    Changed theme

--- lua/pluginlist.lua
+++ lua/pluginlist.lua
@@ -5,13 +5,51 @@ return {
 	"mhartington/formatter.nvim",
 	"mfussenegger/nvim-lint",
 	{
-		"navarasu/onedark.nvim",
+		"rebelot/kanagawa.nvim",
+		lazy = false,
 		priority = 1000,
 		config = function()
-			require("onedark").setup({
-                style="darker"
+			require("kanagawa").setup({
+				compile = false, -- enable compiling the colorscheme
+				undercurl = true, -- enable undercurls
+				commentStyle = { italic = true },
+				functionStyle = {},
+				keywordStyle = { italic = true },
+				statementStyle = { bold = true },
+				typeStyle = {},
+				transparent = false, -- do not set background color
+				dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+				terminalColors = true, -- define vim.g.terminal_color_{0,17}
+				colors = { -- add/modify theme and palette colors
+					palette = {},
+					theme = {
+						wave = {},
+						lotus = {},
+						dragon = {},
+						all = {
+							ui = { bg_gutter = "#171a20", bg = "#22262e" },
+						},
+					},
+				},
+				overrides = function(colors) -- add/modify highlights
+					local theme = colors.theme
+					return {
+						Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+						PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+						PmenuSbar = { bg = theme.ui.bg_m1 },
+						PmenuThumb = { bg = theme.ui.bg_p2 },
+						TelescopeTitle = { fg = theme.ui.special, bold = true },
+						TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+						TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+						TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+						TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+						TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+						TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+					}
+				end,
+				theme = "wave", -- Load "wave" theme when 'background' option is not set
 			})
-			vim.cmd("colorscheme onedark")
+			vim.cmd.colorscheme("kanagawa")
 		end,
 	},
 	{
